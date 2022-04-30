0e6413ef1fb0e75efaff4c0e9d65d451d973ce79
adhiman6 <adhiman6@gatech.edu>
2022-05-01 00:23:18 +0530

    passing correct order of values in loss func

--- model/lossFunction.py
+++ model/lossFunction.py
@@ -2,8 +2,21 @@ import torch
 import torchvision
 
 
-def inverseHuberLoss(target, out):
-    diff = torch.abs(out - target)
-    Coff = 0.2 * torch.max(diff).item()
-    return torch.mean(torch.where(diff < Coff, diff, (diff ^ 2 + Coff ^ 2) / (2 * Coff)))
+def inverseHuberLoss(out, target):
+    """
 
+    Parameters
+    ----------
+    out
+    target
+
+    Returns
+    -------
+
+    """
+    # diff = torch.abs(out - target)
+    # Coff = 0.2 * torch.max(diff).item()
+    # return torch.mean(torch.where(diff < Coff, diff, (diff *diff + Coff ^ 2) / (2 * Coff)))
+    absdiff = torch.abs(out - target)
+    C = 0.2 * torch.max(absdiff).item()
+    return torch.mean(torch.where(absdiff < C, absdiff, (absdiff * absdiff + C * C) / (2 * C)))
