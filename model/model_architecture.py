827c9454c79371d8c60b14b7c3f85a674ee7eab0
Jai Motwani <jmotwani7@gatech.edu>
2022-05-01 19:13:32 +0530

    alexnet based model

--- model/model_architecture.py
+++ model/model_architecture.py
@@ -25,11 +25,48 @@ class Resnet50BasedModel(nn.Module):
             for param in resnet50.parameters():
                 param.requires_grad = False
 
+        print("--------ResNet50 Based--------")
         # remove until the resnet-50 AvgPool Layer
         self.model = nn.Sequential(*(list(resnet50.children())[:-2]),
                                    nn.Conv2d(2048, 1024, (1, 1)),
                                    nn.BatchNorm2d(1024), FastUpConvolution(1024, 512),
                                    FastUpConvolution(512, 256),
+                                   FastUpConvolution(256, 128),
+                                   FastUpConvolution(128, 64),
+                                   nn.Dropout2d(),
+                                   nn.Conv2d(64, 1, 3, padding=1, padding_mode='zeros'),
+                                   nn.ReLU(),
+                                   nn.Upsample(size=(228, 304), mode='bilinear'))
+        print(f'Trainable parameters for {self._get_name()} => {get_trainable_parameters(self.model)}')
+
+    def forward(self, input_tensor):
+        # input_tensor.to(self.device)
+        """Runs a forward pass over the Model"""
+        output_tensor = self.model(input_tensor)
+        return output_tensor
+
+
+class AlexNetBasedModel(nn.Module):
+    """"
+    Alexnet Based model which accepts input of size 304 X 228 X 3
+    And generates a prediction of 160 X 128 X 1
+    Architecture :
+        AlexNet Base Model ( without FC layers ) , followed by UnConvolutionLayers
+    """
+
+    def __init__(self, device='cpu', freeze_decoder_weights=False):
+        super(AlexNetBasedModel, self).__init__()
+        alexnet = torchvision.models.alexnet(pretrained=True)
+        self.freeze_decoder_weights = freeze_decoder_weights
+        self.device = device
+
+        if self.freeze_decoder_weights:
+            for param in alexnet.parameters():
+                param.requires_grad = False
+        print("--------AlexNet Based--------")
+        self.model = nn.Sequential(*(list(alexnet.children())[:-2]),
+                                   nn.Conv2d(256, 256, 3, padding=1, padding_mode='zeros'),
+                                   nn.BatchNorm2d(256),
                                    FastUpConvolution(256, 128),
                                    FastUpConvolution(128, 64),
                                    nn.Dropout2d(),
