3cb89386d6f96027cb104968b81caf1b2080d841
JuJu-uuu <103913831+JuJu-uuu@users.noreply.github.com>
2022-05-03 19:48:44 +0800

    cityscape dataloader

--- utils/datasetutil.py
+++ utils/datasetutil.py
@@ -10,15 +10,17 @@ import numpy as np
 from PIL import Image
 import os
 from augmentations import augmentations_new
+from skimage import transform
 
 
 class CityScapeGenerator(torch.utils.data.Dataset):
-    def __init__(self, root, split="train"):
+    def __init__(self, root, split="train",  augment_data=True):
         self.root = root
         self.split = split
+        self.augment_data = augment_data
         self.files = {}
         for split in ["train", "val"]:
-            file_list = glob.glob(root + "/" + split + "/" + split + "_image/**")
+            file_list = glob.glob(root + "/" + split + "/" + "image/**")
             self.files[split] = file_list
 
     def __len__(self):
@@ -28,21 +30,25 @@ class CityScapeGenerator(torch.utils.data.Dataset):
     def __getitem__(self, id):
         img_path = self.files[self.split][id]
         img_name = os.path.split(self.files[self.split][0])[-1][:-4]
-        dpt_path = self.root + "/" + self.split + "/" + self.split + "_depth/" + img_name + ".jpg"
+        dpt_path = self.root + "/" + self.split + "/" + "depth/" + img_name + ".jpg"
         img = np.asarray(Image.open(img_path))
         dpt = np.asarray(Image.open(dpt_path))
-        input_transform = augmentations_new.Compose([augmentations_new.Scale(228),
-                                                     augmentations_new.ArrayToTensor()])
+        img = transform.resize(img, (480,640))
+        dpt = transform.resize(dpt, (480,640))
+        if self.augment_data:
+            img_dep_transform = augmentations_new.Compose([augmentations_new.RandomVerticalFlip(),
+                                                           augmentations_new.RandomHorizontalFlip(),
+                                                           augmentations_new.RandomRotate(15),
+                                                           augmentations_new.AorB(augmentations_new.Scale(228), augmentations_new.RandomCrop((228, 304)), probA=0.8),
+                                                           augmentations_new.ArrayToTensor()
+                                                           ])
+        else:
+            img_dep_transform = transforms.Compose([augmentations_new.Scale(228),
+                                                    augmentations_new.ArrayToTensor()])
+        img, dpt = img_dep_transform(img, dpt)
 
-        img, dpt = input_transform(img, dpt)
+        return img / 255., dpt
 
-        return img.transpose((2, 1, 0)) / 255., dpt
-
-    def transform(self, img, label):
-        img = Image.fromarray(img).resize((480, 640), Image.ANTIALIAS)
-        label = Image.fromarray(label).resize((480, 640), Image.ANTIALIAS)
-
-        return np.asarray(img), np.asarray(label)
 
 
 class NyuDatasetLoader(Dataset):
@@ -101,9 +107,9 @@ def save_test_train_ids(file_path, train_percent=0.8, last_id=1448):
         f.write(json.dumps(ids_dict))
 
 
-def get_nyuv2_test_train_dataloaders(dataset_path, train_ids, val_ids, test_ids, batch_size=3, apply_augmentations=True):
-    return DataLoader(NyuDatasetLoader(dataset_path, train_ids, augment_data=apply_augmentations), batch_size, shuffle=True), \
-           DataLoader(NyuDatasetLoader(dataset_path, val_ids, augment_data=apply_augmentations), batch_size, shuffle=True), \
+def get_nyuv2_test_train_dataloaders(dataset_path, train_ids, val_ids, test_ids, batch_size=32):
+    return DataLoader(NyuDatasetLoader(dataset_path, train_ids), batch_size, shuffle=True), \
+           DataLoader(NyuDatasetLoader(dataset_path, val_ids), batch_size, shuffle=True), \
            DataLoader(NyuDatasetLoader(dataset_path, test_ids), batch_size, shuffle=True)
 
 
